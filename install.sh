c1f20d8e7f7b5b5d8789def145190a12545dea96
Berilo Wanderley <wberilo@gmail.com>
2021-06-22 19:00:47 -0300

    ARM

--- install.sh
+++ install.sh
@@ -36,8 +36,8 @@ sed -i "s/<add_your_token_here>/$1/g" /opt/ngrok/ngrok.yml
 
 cd /opt/ngrok
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip
-rm ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-arm.zip
+rm ngrok-stable-linux-arm.zip
 chmod +x ngrok
 
 systemctl enable ngrok.service
